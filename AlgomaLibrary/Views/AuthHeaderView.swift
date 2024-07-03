d31a34b8cfc7c6d42ff17b14ff7e5e0e3ae3e54d
fadebo <fadebo@algomau.ca>
2024-07-03 16:25:48 -0400

    ..

---
+++ AlgomaLibrary/Views/AuthHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  AuthHeaderView.swift
+//  AlgomaLibrary
+//
+//  Created by Lab8student on 2024-07-03.
+//
+
+import UIKit
+
+class AuthHeaderView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
